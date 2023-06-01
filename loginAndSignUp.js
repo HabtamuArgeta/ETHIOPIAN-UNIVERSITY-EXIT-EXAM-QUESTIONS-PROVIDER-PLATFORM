d51d2ad02418c28c1aa85ed2e7dc811c587b8d45
nokosha-mani <argetahabtamu8@gmail.com>
2023-06-01 17:44:15 +0200

    login for admin added

---
+++ loginAndSignUp.js
@@ -0,0 +1,9 @@
+function handelError(error){
+ document.getElementById('error').innerHTML=error;
+}
+function redirectPageToHome() {
+    window.location.href = "home.html";
+  }
+  function redirectPageToLogin() {
+    window.location.href = "index.html";
+  }
