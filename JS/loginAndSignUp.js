36b3d6e8dcd7bd5729c37824224623bdba500a87
nokosha-mani <argetahabtamu8@gmail.com>
2023-06-01 21:28:36 +0200

    collected to named file

--- JS/loginAndSignUp.js
+++ JS/loginAndSignUp.js
@@ -2,8 +2,8 @@ function handelError(error){
  document.getElementById('error').innerHTML=error;
 }
 function redirectPageToHome() {
-    window.location.href = "home.html";
+    window.location.href = "../HTML/home.html";
   }
   function redirectPageToLogin() {
-    window.location.href = "index.html";
+    window.location.href = "../HTML/index.html";
   }
