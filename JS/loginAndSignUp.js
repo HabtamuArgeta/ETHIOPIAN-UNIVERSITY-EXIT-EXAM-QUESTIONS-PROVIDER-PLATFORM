8e5a2a48b5772727acf55dea0719b3fdb9eee1a0
nokosha-mani <argetahabtamu8@gmail.com>
2023-06-17 18:34:31 +0200

    working page without media querry

--- JS/loginAndSignUp.js
+++ JS/loginAndSignUp.js
@@ -2,7 +2,7 @@ function handelError(error){
  document.getElementById('error').innerHTML=error;
 }
 function redirectPageToHome() {
-    window.location.href = "../HTML/home.html";
+    window.location.href = "../HTML/AdminHome.html";
   }
   function redirectPageToLogin() {
     window.location.href = "../HTML/index.html";
